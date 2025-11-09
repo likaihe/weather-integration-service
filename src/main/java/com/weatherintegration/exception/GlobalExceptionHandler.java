3fd19e60d12cc6c83ebc61793ec4530ccdf2fbb0
likai <1047561425@qq.com>
2025-11-10 09:59:32 +1300

    improve

--- src/main/java/com/weatherintegration/exception/GlobalExceptionHandler.java
+++ src/main/java/com/weatherintegration/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.weatherintegration.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -25,6 +27,30 @@ public class GlobalExceptionHandler {
     public com.weatherintegration.model.Error handleOpenWeatherException(OpenWeatherException ex) {
         return new com.weatherintegration.model.Error().code(("BAD_GATEWAY")).message(ex.getMessage());
     }
+
+
+    @ExceptionHandler(BindException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public com.weatherintegration.model.Error handleBindException(BindException ex, HttpServletRequest req) {
+        //todo improve ERROR to have details of fields
+        return new com.weatherintegration.model.Error().code(("BAD_REQUEST")).message(ex.getMessage());
+    }
+
+    @ExceptionHandler(jakarta.validation.ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public com.weatherintegration.model.Error handleConstraintViolation(jakarta.validation.ConstraintViolationException ex) {
+        return new com.weatherintegration.model.Error().code(("BAD_REQUEST")).message(ex.getMessage());
+    }
+
+
+//    //todo improve
+//    @ExceptionHandler(org.springframework.web.method.annotation.MethodArgumentTypeMismatchException.class)
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    public com.weatherintegration.model.Error handleTypeMismatch(jakarta.validation.ConstraintViolationException ex) {
+//        return new com.weatherintegration.model.Error().code(("BAD_REQUEST")).message(ex.getMessage());
+//    }
+
+
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
